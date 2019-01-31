a9961289b63f93017241c6e4163f7154b132c872
s0511527 <troy.gidney@lethbridgecollege.ca>
2019-01-30 17:17:48 -0700

    I want to die

--- src/me/troyg/assignment4/main.java
+++ src/me/troyg/assignment4/main.java
@@ -6,6 +6,8 @@ public class main {
     public static void main(String[] args) {
         GUI.jFrame = new JFrame("File Formatter");
         GUI.jFrame.setContentPane(new GUI().jPanel);
+        GUI.jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        GUI.jFrame.setResizable(false);
         GUI.jFrame.pack();
         GUI.jFrame.setVisible(true);
     }
